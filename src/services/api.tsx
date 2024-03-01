8be4a88b1cf2244c6e38a8d4cb1d404aa030bb7f
Daniel Kriade <daniel_kraide@hotmail.com>
2024-03-01 13:42:04 -0300

    corrigido link api

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-   baseURL: "https://krdapi.emartim.com.br/api"
+   baseURL: "https://backendpdv.azurewebsites.net/api"
 });
 
 export {api};
