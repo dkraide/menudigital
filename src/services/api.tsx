d47bc32ee19bd28cf1892687be11efcdbe9ca1a2
Daniel Kriade <daniel_kraide@hotmail.com>
2024-04-30 13:16:34 -0300

    ttt

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-   //baseURL: "https://backendpdv.azurewebsites.net/api"
-   baseURL: "https://localhost:3334/api"
+   baseURL: "https://backendpdv.azurewebsites.net/api"
+   //baseURL: "https://localhost:3334/api"
 });
 
 export {api};
