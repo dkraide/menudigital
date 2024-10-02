21cb389845400a4f10dde6e81e82ffa9509cfdda
Daniel Kriade <daniel_kraide@hotmail.com>
2024-10-02 13:55:29 -0300

    lnik apoi pra variar

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: "https://pdv.krdsys.tech/api"
-   baseURL: "http://localhost:7000/api"
+   baseURL: "https://pdv.krdsys.tech/api"
+  // baseURL: "http://localhost:7000/api"
 });
 
 export {api};
